b24059e4d0e9aa5c4502a8ede7ff05d986609666
soy <catpotd@gmail.com>
2024-05-21 08:54:03 +0900

    chore: add cesium to peerDependencies and update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,12 @@ import svgr from "vite-plugin-svgr";
 import { configDefaults } from "vitest/config";
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  plugins: [svgr(), react(), cesium({ rebuildCesium: true }), dts({ rollupTypes: true })],
+  plugins: [
+    svgr(),
+    react(),
+    cesium({ rebuildCesium: true }),
+    dts({ rollupTypes: true, exclude: ["./src/env.d.ts"] }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
