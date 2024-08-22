d1ddde33badc26b0eb23a1fe7a7b12fc7b231b69
airslice <icesunex@hotmail.com>
2024-08-22 08:47:59 +0800

    chore: remove token

--- example/token.ts
+++ example/token.ts
@@ -1,3 +1,2 @@
 // Ref: https://github.com/CesiumGS/cesium/blob/main/packages/engine/Source/Core/Ion.js#L7
-export const CESIUM_ION_ACCESS_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxY2UxZDdkOS02YTJkLTQ2Y2EtYmMxNS03ZDI2NWQ1NzUzMjAiLCJpZCI6MjU5LCJpYXQiOjE3MjI1Mjg0MTl9.IMe1VGs-5blTCJJA1y76M8WNixMgms2C87kIkXQVcww";
+export const CESIUM_ION_ACCESS_TOKEN = "";
