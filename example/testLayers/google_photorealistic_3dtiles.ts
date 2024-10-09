55236cba35a894f7a949becf240ebbc358f95266
mkumbobeaty <charsbeaty@gmail.com>
2024-10-09 18:36:54 +0300

    added test layer

---
+++ example/testLayers/google_photorealistic_3dtiles.ts
@@ -0,0 +1,13 @@
+import { Layer } from "@reearth/core";
+
+export const GOOGLE_PHOTOREALISTIC_3DTILES: Layer = {
+  id: "google_photorealistic_3dtiles",
+  type: "simple",
+  data: {
+    type: "google-photorealistic",
+    serviceTokens: {
+      googleMapApiKey: import.meta.env.VITE_GOOGLE_MAP_API_KEY || "",
+    },
+  },
+  "3dtiles": {},
+};
--- example/testLayers/index.ts
+++ example/testLayers/index.ts
@@ -2,6 +2,12 @@ import { Layer } from "@reearth/core";
 
 import { GEOJSON_MARKER } from "./geojson_marker";
 import { GEOJSON_SIMPLE } from "./geojson_simple";
+import { GOOGLE_PHOTOREALISTIC_3DTILES } from "./google_photorealistic_3dtiles";
 import { LAND_USE } from "./mvt";
 
-export const TEST_LAYERS: Layer[] = [LAND_USE, GEOJSON_MARKER, GEOJSON_SIMPLE];
+export const TEST_LAYERS: Layer[] = [
+  LAND_USE,
+  GEOJSON_MARKER,
+  GEOJSON_SIMPLE,
+  GOOGLE_PHOTOREALISTIC_3DTILES,
+];
