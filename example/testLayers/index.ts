6042d64cf869f534f26bbad45584b88a448e8050
airslice <icesunex@hotmail.com>
2024-10-11 11:11:34 +0800

    refactor: add test layers

--- example/testLayers/index.ts
+++ example/testLayers/index.ts
@@ -4,10 +4,14 @@ import { GEOJSON_MARKER } from "./geojson_marker";
 import { GEOJSON_SIMPLE } from "./geojson_simple";
 import { GOOGLE_PHOTOREALISTIC_3DTILES } from "./google_photorealistic_3dtiles";
 import { LAND_USE } from "./mvt";
+import { OSM_BUILDINGS } from "./osm_buildings";
+import { THREEDTILES_SIMPLE } from "./threedtiles_simple";
 
 export const TEST_LAYERS: Layer[] = [
   LAND_USE,
   GEOJSON_MARKER,
   GEOJSON_SIMPLE,
   GOOGLE_PHOTOREALISTIC_3DTILES,
+  OSM_BUILDINGS,
+  THREEDTILES_SIMPLE,
 ];
---
+++ example/testLayers/threedtiles_simple.ts
@@ -0,0 +1,11 @@
+import { Layer } from "@reearth/core";
+
+export const THREEDTILES_SIMPLE: Layer = {
+  id: "3dtiles_simple",
+  type: "simple",
+  data: {
+    type: "3dtiles",
+    url: "https://assets.cms.plateau.reearth.io/assets/4f/702958-5009-4d6b-a2e0-157c7e573eb2/13100_tokyo23-ku_2022_3dtiles _1_1_op_bldg_13101_chiyoda-ku_lod2_no_texture/tileset.json",
+  },
+  "3dtiles": {},
+};
