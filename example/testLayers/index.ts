cfe676911648546381bb977bc3384a255fd9f4f4
airslice <icesunex@hotmail.com>
2025-10-01 16:48:07 +0800

    refactor: update example project

--- example/testLayers/index.ts
+++ example/testLayers/index.ts
@@ -8,6 +8,7 @@ import { LAND_USE, LSLD_SAPPORO, LSLD_NIJIMA } from "./mvt";
 import { OSM_BUILDINGS } from "./osm_buildings";
 import { THREEDTILES_KUMAGAYA_LOD2 } from "./threedtiles_lod2";
 import { THREEDTILES_SIMPLE } from "./threedtiles_simple";
+import { THREEDTILES_SIMPLE_WITH_STYLE_URL } from "./threedtiles_simple_with_style_url";
 
 export const TEST_LAYERS: Layer[] = [
   LAND_USE,
@@ -18,6 +19,7 @@ export const TEST_LAYERS: Layer[] = [
   GOOGLE_PHOTOREALISTIC_3DTILES,
   OSM_BUILDINGS,
   THREEDTILES_SIMPLE,
+  THREEDTILES_SIMPLE_WITH_STYLE_URL,
   THREEDTILES_KUMAGAYA_LOD2,
   CZML_SIMPLE,
 ];
